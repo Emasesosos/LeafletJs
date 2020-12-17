ba33e67ccdcfaf98b7d64d5745b57bcf9fb502ef
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-12-17 14:38:03 -0600

    Configuración Inicial React e Instalación de librerías leaflet y react-leaflet

---
+++ react-leaflet-example/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { MapView } from './components/MapView';
+
+function App() {
+  return (
+    <MapView />
+  );
+}
+
+export default App;
---
+++ react-leaflet-example/src/components/MapView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MapView = () => {
+
+    return (
+        <h1>Map</h1>
+    );
+
+};
